4b732eb25dee186b682fc409098d212ed0c86f0d
Nithish Ragav <53393283+nnr2611@users.noreply.github.com>
2025-07-15 00:08:58 -0600

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -49,7 +49,7 @@ export function Hero() {
                   enableOnHover={true}
                   className='custom-class'
                 >
-                  Chidambaram
+                  Nithish
                 </GlitchText> */}
               </h1>
 
