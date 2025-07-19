680f788e66bd8973f8581d2db9ed72118e6ad17e
Nithish Ragav <53393283+nnr2611@users.noreply.github.com>
2025-07-18 22:44:32 -0600

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,19 +6,19 @@ export function Footer() {
     <footer className="sticky z-40 w-full border-t py-8 md:py-12">
       <div className="container flex flex-col md:flex-row justify-between items-center">
         <div className="mb-4 md:mb-0">
-          <p className="text-sm text-muted-foreground">© {new Date().getFullYear()} Anirudh Chidambaram. All rights reserved.</p>
+          <p className="text-sm text-muted-foreground">© {new Date().getFullYear()} Nithish Ragav Narayana Shankar. All rights reserved.</p>
         </div>
 
         <div className="flex items-center space-x-4">
-          <Link href="https://github.com/RootSlayr" target="_blank" rel="noopener, noreferrer" className="text-muted-foreground hover:text-primary transition-colors">
+          <Link href="https://github.com/nnr2611" target="_blank" rel="noopener, noreferrer" className="text-muted-foreground hover:text-primary transition-colors">
             <Github className="h-5 w-5" />
             <span className="sr-only">GitHub</span>
           </Link>
-          <Link href="https://www.linkedin.com/in/anirudh-chida/" target="_blank" rel="noopener, noreferrer" className="text-muted-foreground hover:text-primary transition-colors">
+          <Link href="www.linkedin.com/in/nithish-ragav-narayana-shankar-6a1a491a5" target="_blank" rel="noopener, noreferrer" className="text-muted-foreground hover:text-primary transition-colors">
             <Linkedin className="h-5 w-5" />
             <span className="sr-only">LinkedIn</span>
           </Link>
-          <Link href="https://www.instagram.com/anirudhan.me?igsh=bjd2azg2eGNyejRs" target="_blank" rel="noopener, noreferrer" className="text-muted-foreground hover:text-primary transition-colors">
+          <Link href="https://www.instagram.com/nithish_ragav/?hl=en" target="_blank" rel="noopener, noreferrer" className="text-muted-foreground hover:text-primary transition-colors">
             <Instagram className="h-5 w-5" />
             <span className="sr-only">Instagram</span>
           </Link>
